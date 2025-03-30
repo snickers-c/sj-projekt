e50447b186844f8352efb3e3ca1d5bd2a31c3a3d
matus <1matus.gabas@gmail.com>
2025-03-30 19:20:40 +0200

    pridanie header.php, footer.php, functions.php

---
+++ _inc/functions.php
@@ -0,0 +1,44 @@
+<?php
+
+function load_menu(array $pages)
+{
+  $menuItems = "";
+
+  foreach ($pages as $page_name => $page_url) {
+    $menuItems .= "<li class=\"scroll-to-section\"><a href=\"$page_url\">$page_name</a></li>";
+  }
+
+  return $menuItems;
+}
+
+function add_stylesheets()
+{
+  $page_name = basename($_SERVER["SCRIPT_NAME"], ".php");
+  echo '<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">';
+  echo '<link rel="stylesheet" href="assets/css/fontawesome.css">';
+  echo '<link rel="stylesheet" href="assets/css/templatemo-scholar.css">';
+  echo '<link rel="stylesheet" href="assets/css/animate.css">';
+  echo '<link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />';
+
+  switch ($page_name) {
+    case "index":
+      echo '<link rel="stylesheet" href="assets/css/owl.css">';
+      break;
+  }
+}
+
+function add_scripts()
+{
+  $page_name = basename($_SERVER["SCRIPT_NAME"], ".php");
+  echo '<script src="vendor/jquery/jquery.min.js"></script>';
+  echo '<script src="vendor/bootstrap/js/bootstrap.min.js"></script>';
+  echo '<script src="assets/js/isotope.min.js"></script>';
+
+  switch ($page_name) {
+    case "index":
+      echo '<script src="assets/js/owl-carousel.js"></script>';
+      echo '<script src="assets/js/custom.js"></script>';
+      echo '<script src="assets/js/counter.js"></script>';
+      break;
+  }
+}
---
+++ components/header.php
@@ -0,0 +1,89 @@
+<?php
+require_once("_inc/functions.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+    rel="stylesheet">
+
+  <title>Scholar - Online School HTML5 Template</title>
+
+  <!-- Bootstrap core CSS -->
+  <!-- Additional CSS Files -->
+  <?php
+  add_stylesheets();
+  ?>
+  <!--
+
+TemplateMo 586 Scholar
+
+https://templatemo.com/tm-586-scholar
+
+-->
+</head>
+
+<body>
+
+  <!-- ***** Preloader Start ***** -->
+  <div id="js-preloader" class="js-preloader">
+    <div class="preloader-inner">
+      <span class="dot"></span>
+      <div class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  </div>
+  <!-- ***** Preloader End ***** -->
+
+  <!-- ***** Header Area Start ***** -->
+  <header class="header-area header-sticky">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <nav class="main-nav">
+            <!-- ***** Logo Start ***** -->
+            <a href="index.html" class="logo">
+              <h1>Scholar</h1>
+            </a>
+            <!-- ***** Logo End ***** -->
+            <!-- ***** Serach Start ***** -->
+            <div class="search-input">
+              <form id="search" action="#">
+                <input type="text" placeholder="Type Something" id='searchText' name="searchKeyword"
+                  onkeypress="handle" />
+                <i class="fa fa-search"></i>
+              </form>
+            </div>
+            <!-- ***** Serach Start ***** -->
+            <!-- ***** Menu Start ***** -->
+            <ul class="nav">
+              <?php
+              $pages = array(
+                "Home" => "#top",
+                "Services" => "#services",
+                "Courses" => "#courses",
+                "Team" => "#team",
+                "Events" => "#events",
+                "Register Now!" => "#contact"
+              );
+              echo load_menu($pages);
+              ?>
+            </ul>
+            <a class='menu-trigger'>
+              <span>Menu</span>
+            </a>
+            <!-- ***** Menu End ***** -->
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
+  <!-- ***** Header Area End ***** -->
