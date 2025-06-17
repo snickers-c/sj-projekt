9b4f23c62c49a00cbada0f2909f8dbf2adfa0110
matus <1matus.gabas@gmail.com>
2025-06-18 01:07:26 +0200

    podstránka na registráciu signup.php

--- login.php
+++ login.php
@@ -43,6 +43,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
       <input name="password" type="password" class="form-control" placeholder="Password" required>
     </div>
 
+    <a href="signup.php">Don't have account?</a><br>
     <button type="submit" class="btn btn-primary mt-2">Submit</button>
   </form>
 </div>
---
+++ signup.php
@@ -0,0 +1,84 @@
+<?php
+include_once("components/header.php");
+$auth = new Authenticate($db);
+
+if ($auth->isLoggedIn()) {
+  header("Location: admin.php");
+  exit;
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $firstName = $_POST['firstName'];
+  $lastName = $_POST['lastName'];
+  $role = 1;
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+  $repeatPassword = $_POST['repeatPassword'];
+
+  $userCreated = false;
+  $userLogged = false;
+  if ($password == $repeatPassword) {
+    $user = new User($db);
+    $userCreated = $user->createUser($firstName, $lastName, $role, $email, $password);
+  } else {
+    $err = "Password is not equal!";
+  }
+
+  if ($userCreated) {
+    $userLogged = $auth->login($email, $password);
+  } else {
+    $err = $err ?? "Sign up failed!";
+  }
+
+  if ($userLogged) {
+    header("Location: admin.php");
+    exit;
+  } else {
+    $err = $err ?? "Login failed!";
+  }
+}
+?>
+
+<div class="header-text my-5">
+  <br>
+</div>
+<div class="container">
+  <h1>Sign up</h1>
+
+  <?php
+  if (isset($err)) {
+    echo '<br><div class="alert alert-danger" role="alert">' . $err . '</div>';
+    $err = null;
+  }
+  ?>
+
+  <form method="POST">
+    <div class="form-group">
+      <label>First name</label>
+      <input name="firstName" type="text" class="form-control" placeholder="First Name" required>
+    </div>
+    <div class="form-group">
+      <label>Last name</label>
+      <input name="lastName" type="text" class="form-control" placeholder="Last name" required>
+    </div>
+    <div class="form-group">
+      <label>Email</label>
+      <input name="email" type="email" class="form-control" placeholder="Email" required>
+    </div>
+    <div class="form-group">
+      <label>Password</label>
+      <input name="password" type="password" class="form-control" placeholder="Password" required>
+    </div>
+    <div class="form-group">
+      <label>Repeat password</label>
+      <input name="repeatPassword" type="password" class="form-control" placeholder="Repeat password" required>
+    </div>
+
+    <a href="login.php">I have account.</a><br>
+    <button type="submit" class="btn btn-primary mt-2">Submit</button>
+  </form>
+</div>
+
+<?php
+include_once("components/footer.php");
+?>
